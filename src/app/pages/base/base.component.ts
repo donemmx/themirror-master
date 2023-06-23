83c2c06723e47b166fc0e444f58a61f544a43ed6
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-06-23 16:52:04 +0100

    fixed index

--- src/app/pages/base/base.component.ts
+++ src/app/pages/base/base.component.ts
@@ -9,22 +9,21 @@ declare const PaystackPop: any;
 @Component({
   selector: 'app-base',
   templateUrl: './base.component.html',
-  styleUrls: ['./base.component.css']
+  styleUrls: ['./base.component.css'],
 })
 export class BaseComponent implements OnInit, OnDestroy {
   message: { [k: string]: any } = {};
   subscrption: Subscription = new Subscription();
   category$: Observable<any>;
-  learnerId: any
-  learnerApi = inject(LearnersService)
-  learnerAuth = inject(AuthService)
+  learnerId: any;
+  learnerApi = inject(LearnersService);
+  learnerAuth = inject(AuthService);
   constructor(public data: DataService, public router: Router) {}
 
   ngOnInit(): void {
     this.subscrption = this.data.currentMessage.subscribe(
       (message) => (this.message = message)
     );
-
     this.data.currentMessage.subscribe((res: any) => {
       this.category$ = of(res)
       const id: any = this.learnerAuth.getUserId()
@@ -38,8 +37,6 @@ export class BaseComponent implements OnInit, OnDestroy {
             })
           }
     })
-
-  
   }
 
   ngOnDestroy(): void {
@@ -60,58 +57,61 @@ export class BaseComponent implements OnInit, OnDestroy {
     }
   }
 
-  resetUpdate(){
-    this.message.update =null
-    this.data.changeMessage(this.message)
+  resetUpdate() {
+    this.message.update = null;
+    this.data.changeMessage(this.message);
   }
 
-  getLearnerProfile(api: any){
-    api.getLearner({
-      learnerId: this.learnerId
-    }).pipe( shareReplay(1)).subscribe((res: any)=> {
-      this.message.user = res
-      this.data.changeMessage(this.message)
-    }
-    )
+  getLearnerProfile(api: any) {
+    api
+      .getLearner({
+        learnerId: this.learnerId,
+      })
+      .pipe(shareReplay(1))
+      .subscribe((res: any) => {
+        this.message.user = res;
+        this.data.changeMessage(this.message);
+      });
   }
 
+  getUser(api: any, auth: any) {
+    const id: any = auth.getUserId();
+    api.getLearner({ learnerId: id.jti }).subscribe((res: any) => {
+      this.message.user = res;
+      this.data.changeMessage(this.message);
+    });
+  }
 
-  getUser(api: any, auth: any){
-    const id: any = auth.getUserId()
-    api.getLearner({learnerId: id.jti}).subscribe((res: any)=> {
-      this.message.user = res
-      this.data.changeMessage(this.message)
-  })
-} 
-
-payWithPaystack(payload: any, api: any){
-  const paystack = new PaystackPop();
-  paystack.newTransaction({
-    key: payload.key,
-    email: payload.email,
-    amount: payload.amount,
-    ref: payload.reference,
-    onSuccess: (transaction: any) => {
-      console.log(transaction);
-      api
-        .paystackCallback({ paystackId: payload.reference })
-        .subscribe(() => {
-        
-        });
-    },
-    onCancel: () => {
-      // user closed popup
-    },
-})
-}
-updateCourseProgress(api: any, courseId:any, chapterId:any, chapterItemId:any): Observable<any>{
-  return api
-       .addCourseProgress({
-         body: {
-           courseId: courseId,
-           chapterId: chapterId,
-           chapterItemId: chapterItemId,
-         },
-       })
- }
+  payWithPaystack(payload: any, api: any) {
+    const paystack = new PaystackPop();
+    paystack.newTransaction({
+      key: payload.key,
+      email: payload.email,
+      amount: payload.amount,
+      ref: payload.reference,
+      onSuccess: (transaction: any) => {
+        console.log(transaction);
+        api
+          .paystackCallback({ paystackId: payload.reference })
+          .subscribe(() => {});
+      },
+      onCancel: () => {
+        // user closed popup
+      },
+    });
+  }
+  updateCourseProgress(
+    api: any,
+    courseId: any,
+    chapterId: any,
+    chapterItemId: any
+  ): Observable<any> {
+    return api.addCourseProgress({
+      body: {
+        courseId: courseId,
+        chapterId: chapterId,
+        chapterItemId: chapterItemId,
+      },
+    });
+  }
 }
